345e11541776da42d5a7eefb83dca5045e8eb8fb
witulski <John.Witulski@hhu.de>
2019-01-19 12:14:13 +0100

    Exc. number 6 done

---
+++ Solutions/OOP/KaffeeGeschaeft.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class KaffeeGeschaeft
+    {
+        private double _preisProKg;
+
+        public double PreisProKg
+        {
+            get
+            {
+                return _preisProKg;
+            }
+
+            set
+            {
+                if (value >= 5 && value <= 30)
+                {
+                    _preisProKg = value;
+                }
+            }
+        }
+
+        KaffeeGeschaeft(double preisProKg)
+        {
+            PreisProKg = preisProKg;
+        }
+
+        public double KaufeKaffee(Kaffeemaschine maschine, double menge)
+        {
+            double tatsaechlicheMenge = maschine.BohnenAuffuellen(menge);
+            return PreisProKg * tatsaechlicheMenge;
+        }
+    }
+}
