1f428e1bf423aa4755c54f03a03ee33ce60bffe8
witulski <John.Witulski@hhu.de>
2019-01-19 20:29:54 +0100

    Add Equals Method

--- Solutions/OOP/Kaffeemaschine.cs
+++ Solutions/OOP/Kaffeemaschine.cs
@@ -16,32 +16,34 @@
             bohnen = 0;
             wasser = 0;
             gesamtMengeKaffeProduziert = 0;
+            maxBohnen = 100;
+            maxWasser = 100;
         }
 
         public double WasserAuffuellen(double menge)
         {
-            if (wasser+menge<=maxWasser)
+            if (Wasser+menge<=maxWasser)
             {
-                wasser += menge;
+                Wasser += menge;
                 return menge;
             } 
             else
             {
-                wasser = maxWasser;
+                Wasser = maxWasser;
                 return maxWasser - wasser;
             }
         }
 
         public double BohnenAuffuellen(double menge)
         {
-            if (bohnen + menge <= maxBohnen)
+            if (Bohnen + menge <= maxBohnen)
             {
-                bohnen += menge;
+                Bohnen += menge;
                 return menge;
             }
             else
             {
-                bohnen = maxBohnen;
+                Bohnen = maxBohnen;
                 return maxBohnen - bohnen;
             }
         }
@@ -73,8 +75,42 @@
             {
                 bohnen -= mengeBohnen;
                 wasser -= mengeWasser;
+                gesamtMengeKaffeProduziert += menge;
                 return true;
             }
+        }
+
+        public override bool Equals(object other)
+        {
+            if(other is Kaffeemaschine)
+            {
+                Kaffeemaschine m = other as Kaffeemaschine;
+                return m.Bohnen == this.Bohnen && m.Wasser == this.Wasser;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return int.Parse(Wasser.ToString()+Bohnen.ToString()); // FIXME: performance
+        }
+
+        public static bool operator ==(Kaffeemaschine k0, Kaffeemaschine k1)
+        {
+            if (!(k0 is null))
+            {
+                return k0.Equals(k1);
+            }
+            if (!(k1 is null))
+            {
+                return k1.Equals(k0);
+            }
+            return false;
+        }
+
+        public static bool operator !=(Kaffeemaschine k0, Kaffeemaschine k1)
+        {
+            return !(k0==k1);
         }
     }
 }
--- Solutions/OOP/OOP.cs
+++ Solutions/OOP/OOP.cs
@@ -6,16 +6,42 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            TestComplex();
+            TestPerson();
+            TestKaffee();
+        }
+
+        static void TestComplex()
+        {
             var a = new Complex(7, 4);
             var b = new Complex(3, 5);
             var c = a + b;
             Console.WriteLine(c);
+        }
+
+        static void TestPerson()
+        {
             var m = new Mitarbeiter("John", 34, 1234);
             Console.WriteLine(m.Alter);
             int i = 5;
             RefTest(m, ref i);
             Console.WriteLine(i);
             Console.WriteLine(m.Alter);
+        }
+
+        static void TestKaffee()
+        {
+            var k0 = new Kaffeemaschine();
+            k0.BohnenAuffuellen(10);
+            k0.WasserAuffuellen(10);
+            var k1 = new Kaffeemaschine();
+            k1.BohnenAuffuellen(7);
+            k1.WasserAuffuellen(7);
+            var k2 = new Kaffeemaschine();
+            k2.BohnenAuffuellen(10);
+            k2.WasserAuffuellen(10);
+            Console.WriteLine(k0 == k1);
+            Console.WriteLine(k0 == k2);
         }
 
         static void RefTest(Person p, ref int i)
