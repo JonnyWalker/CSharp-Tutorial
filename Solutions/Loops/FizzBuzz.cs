341e9ad8c86c8f18faa70c568e4c37d18e302e5d
witulski <John.Witulski@hhu.de>
2019-01-19 01:20:39 +0100

    Simple loop example

---
+++ Solutions/Loops/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loops
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            uint n = 15;
+
+            for (uint i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
