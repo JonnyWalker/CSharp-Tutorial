617955274bdba7c429637bb5d633a5f7de074e7e
witulski <John.Witulski@hhu.de>
2019-01-23 22:08:54 +0100

    Exception and delegate stuff

---
+++ Solutions/Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var function = GetOperator('+');
+            Console.WriteLine(function(5, 6));
+        }
+
+        static Func<int,int,int> GetOperator(char op)
+        {
+            if(op=='+')
+            {
+                return (a, b) => (a + b);
+            }
+            if (op == '-')
+            {
+                return (a, b) => (a - b);
+            }
+            if (op == '*')
+            {
+                return (a, b) => (a * b);
+            }
+            if (op == '/')
+            {
+                return (a, b) => (a / b);
+            }
+            return (a, b) => (0);
+        }
+    }
+}
---
+++ Solutions/MyExceptionStuff/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyExceptionStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division(42, 0);
+            Console.WriteLine(Fak(6));
+            Console.WriteLine(Fak(-1));
+        }
+
+        static int Division(int a, int b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("¡Ay, ay, ay, no me gusta!");
+                return 0;
+            }     
+        }
+
+        static int Fak(int n)
+        {
+            if(n<0)
+            {
+                throw new ArgumentException("all your base are belong to us");
+            }
+            if (n==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Fak(n - 1);
+            }
+        }
+    }
+}
